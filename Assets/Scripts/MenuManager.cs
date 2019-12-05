dc851063a602ec948ccd01eb03d95981732d9e36
Kapa <tylahkapa@gmail.com>
2019-12-05 20:52:56 +1000

    Some very cool button animations

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MenuManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera mainMenuCam = null;
+    public CinemachineVirtualCamera optionsCam = null;
+
+    public void OptionsButton(){
+        //cam.Follow = optionsTransform;
+        //cam.LookAt = optionsTransform;
+        mainMenuCam.Priority = 0;
+        optionsCam.Priority = 1;
+    }    
+
+    public void MainMenuReturn(){
+        //cam.Follow = menuTransform;
+        //cam.LookAt = menuTransform;
+        optionsCam.Priority = 0;
+        mainMenuCam.Priority = 1;
+    }
+}
