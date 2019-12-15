36768e1f1833668ef93f4c737cf785ec2a0fc228
Kapa <tylahkapa@gmail.com>
2019-12-15 12:00:42 +1000

    no message

---
+++ Assets/OptionsSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsSlider : MonoBehaviour
+{
+    public TextMeshProUGUI sliderText = null;
+    public string text = "New String";
+
+    Slider slider = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(TryGetComponent<Slider>(out Slider result)){
+            slider = result;
+            slider.onValueChanged.AddListener(delegate {UpdateText();});
+            UpdateText();
+        } else {
+            Debug.LogError(gameObject + " doesn't have a slider component");
+        }
+    }
+
+    void UpdateText(){
+        sliderText.text = text + " " + slider.value.ToString("F2");
+    }
+}
